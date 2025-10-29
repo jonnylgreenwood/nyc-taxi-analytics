3a9ba64621a0701cf5dc74df138e8a83dfab8c5d
Jonny Greenwood <jonnylgreenwood@gmail.com>
2025-10-29 14:04:15 +0000

    added location_lookup to l0

--- etl/01_l0/06_load_taxi_zone_lookup.sql
+++ etl/01_l0/06_load_taxi_zone_lookup.sql
@@ -1,8 +1,16 @@
+----------------------------------------------------
+-- Load New York TLC Taxi Zone Shapefile → L0
+----------------------------------------------------
+
+INSTALL spatial;
+LOAD spatial;
+
 CREATE SCHEMA IF NOT EXISTS l0;
 
 CREATE OR REPLACE TABLE l0.taxi_zone_lookup AS
-SELECT *
-FROM read_dbf('data/taxi_zones/taxi_zones.dbf');
-
-DESCRIBE l0.taxi_zone_lookup;
-SELECT * FROM l0.taxi_zone_lookup LIMIT 10;
+SELECT
+    CAST(LocationID AS INTEGER) AS location_id,
+    TRIM(borough) AS borough,
+    TRIM(zone) AS zone,
+    ST_AsText(geom) AS wkt  -- ✅ st_astext is alias for st_aswkt in DuckDB
+FROM st_read('data/taxi_zones/taxi_zones.shp');
