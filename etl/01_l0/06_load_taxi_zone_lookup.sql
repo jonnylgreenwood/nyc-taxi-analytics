17ef743b89ef0a6d9fa1b9d760d1ba1f97c5fea3
Jonny Greenwood <jonnylgreenwood@gmail.com>
2025-10-31 10:19:05 +0000

    Changed to just copy l1 to l2

--- etl/01_l0/06_load_taxi_zone_lookup.sql
+++ etl/01_l0/06_load_taxi_zone_lookup.sql
@@ -1,16 +1,6 @@
-----------------------------------------------------
--- Load New York TLC Taxi Zone Shapefile → L0
-----------------------------------------------------
-
 INSTALL spatial;
 LOAD spatial;
 
-CREATE SCHEMA IF NOT EXISTS l0;
-
 CREATE OR REPLACE TABLE l0.taxi_zone_lookup AS
-SELECT
-    CAST(LocationID AS INTEGER) AS location_id,
-    TRIM(borough) AS borough,
-    TRIM(zone) AS zone,
-    ST_AsText(geom) AS wkt  -- ✅ st_astext is alias for st_aswkt in DuckDB
-FROM st_read('data/taxi_zones/taxi_zones.shp');
+SELECT * 
+FROM ST_Read('data/taxi_zones/taxi_zones.shp');
