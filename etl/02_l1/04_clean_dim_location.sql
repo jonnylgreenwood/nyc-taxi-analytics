10c50c4a3176de154fcddc5a537b7a3a8f568a5d
Jonny Greenwood <jonnylgreenwood@gmail.com>
2025-10-31 10:20:22 +0000

    changed to just copying from l1

--- etl/02_l1/04_clean_dim_location.sql
+++
@@ -1,9 +0,0 @@
-CREATE SCHEMA IF NOT EXISTS l2;
-
-CREATE OR REPLACE TABLE l2.dim_location AS
-SELECT
-    CAST(LocationID AS INTEGER) AS location_id,
-    TRIM(Borough) AS borough,
-    TRIM(Zone) AS zone,
-    TRIM(service_zone) AS service_zone
-FROM l0.taxi_zone_lookup;
