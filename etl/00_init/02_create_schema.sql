3d2c52564ccb35a533f93dc564f0ac5654c2d0e7
Jonny Greenwood <jonnylgreenwood@gmail.com>
2025-11-13 16:41:31 +0000

    db schema defintion

---
+++ etl/00_init/02_create_schema.sql
@@ -0,0 +1,4 @@
+CREATE SCHEMA IF NOT EXISTS l0;  -- raw
+CREATE SCHEMA IF NOT EXISTS l1;  -- cleaned + keys
+CREATE SCHEMA IF NOT EXISTS l2;  -- curated for analytics
+CREATE SCHEMA IF NOT EXISTS dq;  -- data quality quarantine & l
