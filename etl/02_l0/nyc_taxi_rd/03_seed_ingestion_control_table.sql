8a9144577d139ee004fc1a3fbd29c5ccd7126cfa
Jonny Greenwood <jonnylgreenwood@gmail.com>
2025-11-13 16:42:17 +0000

    files to be loaded defined

---
+++ etl/02_l0/nyc_taxi_rd/03_seed_ingestion_control_table.sql
@@ -0,0 +1,43 @@
+INSERT INTO dq.ingestion_plan (service_type, file_month, file_path)
+VALUES
+    -- Yellow
+    ('yellow','2024-10','data/raw/yellow_tripdata_2024-10.parquet'),
+    ('yellow','2024-11','data/raw/yellow_tripdata_2024-11.parquet'),
+    ('yellow','2024-12','data/raw/yellow_tripdata_2024-12.parquet'),
+    ('yellow','2025-01','data/raw/yellow_tripdata_2025-01.parquet'),
+    ('yellow','2025-02','data/raw/yellow_tripdata_2025-02.parquet'),
+    ('yellow','2025-03','data/raw/yellow_tripdata_2025-03.parquet'),
+    ('yellow','2025-04','data/raw/yellow_tripdata_2025-04.parquet'),
+    ('yellow','2025-05','data/raw/yellow_tripdata_2025-05.parquet'),
+    ('yellow','2025-06','data/raw/yellow_tripdata_2025-06.parquet'),
+    ('yellow','2025-07','data/raw/yellow_tripdata_2025-07.parquet'),
+    ('yellow','2025-08','data/raw/yellow_tripdata_2025-08.parquet'),
+    ('yellow','2025-09','data/raw/yellow_tripdata_2025-09.parquet'),
+
+    -- Green
+    ('green','2024-10','data/raw/green_tripdata_2024-10.parquet'),
+    ('green','2024-11','data/raw/green_tripdata_2024-11.parquet'),
+    ('green','2024-12','data/raw/green_tripdata_2024-12.parquet'),
+    ('green','2025-01','data/raw/green_tripdata_2025-01.parquet'),
+    ('green','2025-02','data/raw/green_tripdata_2025-02.parquet'),
+    ('green','2025-03','data/raw/green_tripdata_2025-03.parquet'),
+    ('green','2025-04','data/raw/green_tripdata_2025-04.parquet'),
+    ('green','2025-05','data/raw/green_tripdata_2025-05.parquet'),
+    ('green','2025-06','data/raw/green_tripdata_2025-06.parquet'),
+    ('green','2025-07','data/raw/green_tripdata_2025-07.parquet'),
+    ('green','2025-08','data/raw/green_tripdata_2025-08.parquet'),
+    ('green','2025-09','data/raw/green_tripdata_2025-09.parquet'),
+
+    -- FHV
+    ('fhv','2024-10','data/raw/fhv_tripdata_2024-10.parquet'),
+    ('fhv','2024-11','data/raw/fhv_tripdata_2024-11.parquet'),
+    ('fhv','2024-12','data/raw/fhv_tripdata_2024-12.parquet'),
+    ('fhv','2025-01','data/raw/fhv_tripdata_2025-01.parquet'),
+    ('fhv','2025-02','data/raw/fhv_tripdata_2025-02.parquet'),
+    ('fhv','2025-03','data/raw/fhv_tripdata_2025-03.parquet'),
+    ('fhv','2025-04','data/raw/fhv_tripdata_2025-04.parquet'),
+    ('fhv','2025-05','data/raw/fhv_tripdata_2025-05.parquet'),
+    ('fhv','2025-06','data/raw/fhv_tripdata_2025-06.parquet'),
+    ('fhv','2025-07','data/raw/fhv_tripdata_2025-07.parquet'),
+    ('fhv','2025-08','data/raw/fhv_tripdata_2025-08.parquet'),
+    ('fhv','2025-09','data/raw/fhv_tripdata_2025-09.parquet');
